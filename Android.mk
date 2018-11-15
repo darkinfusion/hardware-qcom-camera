99cca1fee1bdbec2233dc2cccf0e4efb23b6e20f
darkinfusion <daddyfire366@gmail.com>
2018-11-15 15:02:20 -0700

    add

---
+++ Android.mk
@@ -0,0 +1,18 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
+
+ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
+  ifneq ($(strip $(USE_CAMERA_STUB)),true)
+    ifneq ($(BUILD_TINY_ANDROID),true)
+      ifneq ($(USE_VR_CAMERA_HAL), true)
+        ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
+          include $(call all-makefiles-under,$(call my-dir)/msm8998)
+        endif
+      endif
+    endif
+  endif
+endif
+endif
+
+endif
